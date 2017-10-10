37a04f2990e0f035b770e74dab9ebe792f43c89c
Bu4y <amnes1a@windowslive.com>
2017-10-10 14:11:09 +0700

    user save notification

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -2,7 +2,8 @@
 
 module.exports = function (app) {
   // User Routes
-  var users = require('../controllers/users.server.controller');
+  var users = require('../controllers/users.server.controller'),
+    core = require('../../../core/server/controllers/core.server.controller');
 
   // Setting up the users profile api
   app.route('/api/users/me').get(users.me);
@@ -10,6 +11,7 @@ module.exports = function (app) {
   app.route('/api/users/accounts').delete(users.removeOAuthProvider);
   app.route('/api/users/password').post(users.changePassword);
   app.route('/api/users/picture').post(users.changeProfilePicture);
+  app.route('/api/user/notification').put(core.requiresLoginToken, users.notificationUser);
 
   // Finish by binding the user middleware
   app.param('userId', users.userByID);
