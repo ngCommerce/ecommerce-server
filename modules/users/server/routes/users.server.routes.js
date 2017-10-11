e2ce7e8cef5998458b1236241b620e6a8d8194fb
Bu4y <amnes1a@windowslive.com>
2017-10-11 18:26:25 +0700

    update

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -7,9 +7,9 @@ module.exports = function (app) {
 
   // Setting up the users profile api
   app.route('/api/users/me').get(users.me);
-  app.route('/api/users').put(users.update);
+  app.route('/api/users').put(core.requiresLoginToken, users.update);
   app.route('/api/users/accounts').delete(users.removeOAuthProvider);
-  app.route('/api/users/password').post(users.changePassword);
+  app.route('/api/users/password').post(core.requiresLoginToken, users.changePassword);
   app.route('/api/users/picture').post(users.changeProfilePicture);
   app.route('/api/user/notification').put(core.requiresLoginToken, users.notificationUser);
 
